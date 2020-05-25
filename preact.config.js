9126301c592218561563e20e5564a45a29fd90f9
Ricardo <ricardoshhan@gmail.com>
2020-05-24 21:00:34 -0300

    adding script to create env file in production build

--- preact.config.js
+++ preact.config.js
@@ -4,19 +4,17 @@ export default (config, env, helpers) => {
 	config.resolve.modules.push(env.src);
 
 	// dotenv injection
-	if (env.dev) {
-		const { plugin } = helpers.getPluginsByName(config, "DefinePlugin")[0];
-		Object.assign(
-			plugin.definitions,
-			Object.keys(parsed).reduce(
-				(env, key) => ({
-					...env,
-					[`process.env.${key}`]: JSON.stringify(parsed[key]),
-				}),
-				{}
-			)
-		);
-	}
+	const { plugin } = helpers.getPluginsByName(config, "DefinePlugin")[0];
+	Object.assign(
+		plugin.definitions,
+		Object.keys(parsed).reduce(
+			(env, key) => ({
+				...env,
+				[`process.env.${key}`]: JSON.stringify(parsed[key]),
+			}),
+			{}
+		)
+	);
 
 	// if (config.devServer) {
 	// 	config.devServer.proxy = [
