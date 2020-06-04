5d53969f297888bb17ba1150511e6ce131f1f7b4
Ricardo <ricardoshhan@gmail.com>
2020-06-04 02:24:41 -0300

    prerender post urls too

--- prerender-urls.js
+++ prerender-urls.js
@@ -1,58 +1,32 @@
-// const { parsed } = require("dotenv-safe").config();
+const { parsed } = require("dotenv-safe").config();
 
-module.exports = function () {
-	return [
-		{
-			url: "/",
-			title: "Complex concepts in a simple manner",
-		},
-		{
-			url: "/explore",
-			title: "Learn something new exploring our articles",
-		},
-		{
-			url: "/editor",
-			title: "Write something amazing today",
-		},
-		{
-			url: "/login",
-			title: "Write something amazing today",
-		},
-	];
+const staticPages = [
+	{
+		url: "/",
+		title: "Complex concepts in a simple manner",
+	},
+	{
+		url: "/explore",
+		title: "Learn something new exploring our articles",
+	},
+	{
+		url: "/editor",
+		title: "Write something amazing today",
+	},
+	{
+		url: "/login",
+		title: "Write something amazing today",
+	},
+];
+
+module.exports = async function () {
+	const url = parsed["API_URL"];
+	const response = await fetch(`${url}posts`);
+	const { posts } = await response.json();
+	const postsPages = posts.map((post) => ({
+		url: `/post/${post.id}`,
+		title: post.title,
+	}));
+
+	return [...staticPages, ...postsPages];
 };
-
-// const staticPages = [
-// 	{
-// 		url: "/",
-// 		title: "Complex concepts in a simple manner",
-// 	},
-// 	{
-// 		url: "/explore",
-// 		title: "Learn something new exploring our articles",
-// 	},
-// 	{
-// 		url: "/editor",
-// 		title: "Write something amazing today",
-// 	},
-// 	{
-// 		url: "/login",
-// 		title: "Write something amazing today",
-// 	},
-// ];
-
-// module.exports = async function () {
-// 	const url = JSON.stringify(parsed["API_URL"]);
-// 	console.log("URL", url);
-// 	const response = await fetch(`http://localhost:3000/posts`);
-// 	console.log("responsee", response);
-// 	const { posts } = await response.json();
-// 	console.log("posts", posts);
-// 	const postsPages = posts.map((post) => ({
-// 		url: `/post/${post.id}`,
-// 		title: post.title,
-// 	}));
-
-// 	console.log("static pages", staticPages);
-
-// 	return [...staticPages, ...postsPages];
-// };
